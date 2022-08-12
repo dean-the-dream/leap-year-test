89df4dd83540cbb5dbeada9468bdc298de81c5f8
Dean-The-Dream <lawrence_dean@icloud.com>
2022-08-12 00:28:55 -0400

    fix: update javascript

--- leap_year.js
+++ leap_year.js
@@ -1,11 +1,17 @@
+let userYear; 
 let leapYear;
-let year = prompt("Enter a year to see if it is a leap year!");
+// function processYear() {
+//    userYear = document.getElementsByTagName("input")
+    
+//     if (!(year % 4) && !(year % 100) && !(year % 400)) {
+//     leapYear = true;
+//     }
+//     else{
+//         leapYear = false;
+//     }
+// }
+// let button = document.getElementById("submit");
+// button.addEventListener("click", processYear())
 
-let yearNum = Number(year);
-
-if (!(year % 4) && !(year % 100) && !(year % 400)) {
-    leapYear = true;
-}
-else{
-    leapYear = false;
-}
+userYear = document.getElementsByTagName("input")
+console.log(userYear[0].value);
