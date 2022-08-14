fab73b1a4ca384071d6601dacf9603b85335b54c
Dean-The-Dream <lawrence_dean@icloud.com>
2022-08-13 21:24:46 -0400

    add semicolons to javascript, correct leap year logic

--- leap_year.js
+++ leap_year.js
@@ -3,21 +3,30 @@ let userYear;
 let leapYear;
 
 function changeYear() {
-userYear = document.getElementsByTagName("input")[0].value;
-document.getElementById("year").innerHTML = userYear;
-    
+    userYear = document.getElementsByTagName("input")[0].value;
+    document.getElementById("year").innerHTML = userYear;
+    userYearNum = Number(userYear);   
 }
 
-userYearNum = Number(userYear);
 
-function yearResults(){
-    if (!(userYear % 4) && !(userYear % 100) && !(userYear % 400)) { 
-    leapYear = "is a Leap Year!"
+
+function yearResults() {
+    if (!(userYearNum % 4)){
+        if (!(userYearNum % 100)) {
+            if (!(userYearNum % 400)) {
+                leapYear = "is a Leap Year!"; 
+            } else {
+                leapYear = "is not a Leap Year";
+            }
+        } else {
+            leapYear = "is a Leap Year!";
+        }
+    } else {
+        leapYear = "is not a Leap Year";
     }
-    else {
-        leapYear = "is not a Leap Year"
-    }
-    document.getElementById("result").innerHTML = leapYear
+
+    document.getElementById("result").innerHTML = leapYear;
+    document.getElementById("tagline").innerHTML = "Enter another year to try again!";
 }
 
 // document.getElementById("year").onclick = changeYear()
@@ -25,4 +34,4 @@ function yearResults(){
 // userYear = document.getElementsByTagName("input")[0].value
 // document.getElementById("year").innerHTML = userYear
 console.log(userYear);
-console.log(document.getElementById("result").TEXT_NODE) 
+console.log(document.getElementById("result").TEXT_NODE);
